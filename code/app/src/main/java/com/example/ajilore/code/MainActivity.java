52e3ea29e14be5574817adecbf5b0862fe145418
preciousiajilore <ajilore@ualberta.ca>
2025-10-26 23:19:26 -0600

    trying to figure out how to set up navigatibility between fragments, we fixed it! Use this updated version:)

--- code/app/src/main/java/com/example/ajilore/code/MainActivity.java
+++ code/app/src/main/java/com/example/ajilore/code/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.ajilore.code;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -11,9 +10,6 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.ajilore.code.ui.events.EventsFragment;
 import com.example.ajilore.code.ui.history.HistoryFragment;
@@ -23,7 +19,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
-
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,20 +31,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.menu_bottom_nav);
+        bottomNavigationView = findViewById(R.id.menu_bottom_nav);
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment selected_fragment = null;
+
+                int id = item.getItemId();
+
+                if(id == R.id.historyFragment){
+                    selected_fragment = new HistoryFragment();
+                    //Toast.makeText(MainActivity.this, "History", Toast.LENGTH_SHORT).show();
+                } else if(id == R.id.eventsFragment) {
+                    selected_fragment = new EventsFragment();
+                    //Toast.makeText(MainActivity.this, "Events", Toast.LENGTH_SHORT).show();
+                } else if(id == R.id.inboxFragment) {
+                    selected_fragment = new InboxFragment();
+                    //Toast.makeText(MainActivity.this, "Inbox", Toast.LENGTH_SHORT).show();
+                } else if(id == R.id.profileFragment) {
+                    selected_fragment = new ProfileFragment();
+                    //Toast.makeText(MainActivity.this, "Profile", Toast.LENGTH_SHORT).show();
+                }
+
+                if(selected_fragment != null){
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.nav_host_fragment, selected_fragment).commit();
+                }
+                return true;
+            }
+        });
 
 
-        //NavController  from the host fragment
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-
-        //Hook it up to the bottom nav view
-        NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
 
 
