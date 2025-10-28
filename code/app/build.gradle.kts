2d4df98b3b5ebfaa3184bb254e8f4998dee554b6
preciousiajilore <ajilore@ualberta.ca>
2025-10-28 17:38:49 -0600

    Revert "updated my branch"
    This reverts commit 91ce746b72783a0112dc36046bc575c66f8afb49, reversing
    changes made to 1730403cb1a69f04e8c4d4f9b500637dbedaee57.

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    id("com.google.gms.google-services")
 }
 
 android {
@@ -49,6 +48,4 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation("com.google.android.material:material:1.12.0")
-    implementation(platform("com.google.firebase:firebase-bom:34.3.0"))
-    implementation("com.google.firebase:firebase-firestore")
 }
