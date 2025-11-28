096ab6bd1098789c25bad1aff02a4bcd8c5ca4c7
takindel <takindel@ualberta.ca>
2025-11-27 18:12:23 -0700

    fixed crashing issue

--- code/app/src/main/java/com/example/ajilore/code/ui/profile/LoginFragment.java
+++ code/app/src/main/java/com/example/ajilore/code/ui/profile/LoginFragment.java
@@ -217,7 +217,6 @@ public class LoginFragment extends Fragment {
 
             requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
 
-            //tempUserData = user; // store until permission comes back
             return;
         }
 
